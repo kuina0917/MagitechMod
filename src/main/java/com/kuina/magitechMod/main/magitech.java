a2e9255d56fabbf4e0f2d5179c4922b4fec4bdf6
kuinnkuina <87004197+kuinnkuina@users.noreply.github.com>
2025-04-18 19:56:39 +0900

    failed

--- src/main/java/com/kuina/magitechMod/main/magitech.java
+++ src/main/java/com/kuina/magitechMod/main/magitech.java
@@ -5,7 +5,7 @@ import com.kuina.magitechMod.regi.tab.magitechallitems;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import software.bernie.geckolib.GeckoLib;
+
 
 
 @SuppressWarnings("removal")
@@ -15,7 +15,6 @@ public class magitech {
     public static final String MOD_ID = "magitech";
 
     public magitech() {
-        GeckoLib.initialize();
 
 
             IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
